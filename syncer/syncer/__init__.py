metno/nir

commit 08e11b7a7150aa09565ef6203b0ea211d0d15c3b

Always increment data version by at least one when downloading a model run
Summary: When reloading model runs, it is an advantage if the data version is automatically incremented. This patch introduces the concept of internal model run versions. Fixes T616.

Test Plan: New tests have been added. Run the Syncer test suite.

Reviewers: #nir, siris

Reviewed By: #nir, siris

Projects: #nir

Maniphest Tasks: T616

Differential Revision: https://phab.met.no/D353

syncer/syncer/__init__.py
@@ -91,6 +91,9 @@ class Model(syncer.utils.SerializeBase):
         self.wdb_updated = None
         self.wdb2ts_updated = None
 
+        # Internal version increments of datasets
+        self.model_run_version = {}
+
     @staticmethod
     def data_from_config_section(config, section_name):
         """Return config options for a model. Raise exception if mandatory config option is missing"""
@@ -198,6 +201,35 @@ class Model(syncer.utils.SerializeBase):
         entries required by the data_file_count configuration option for this model.
         """
         return len(self.get_matching_data(dataset)) == self.data_file_count
+
+    def get_model_run_key(self, model_run):
+        """
+        Return a compound key used for identifying a unique reference time and
+        data provider combination used in a specific model run.
+        """
+        return (model_run.data_provider, model_run.reference_time)
+
+    def set_model_run_version(self, model_run, version):
+        """
+        Set the internal version of a model run.
+        """
+        key = self.get_model_run_key(model_run)
+        self.model_run_version[key] = version
+
+    def get_model_run_version(self, model_run):
+        """
+        Return the internal version of the specified model run.
+        """
+        key = self.get_model_run_key(model_run)
+        if key not in self.model_run_version:
+            return 0
+        return self.model_run_version[key]
+
+    def increment_model_run_version(self, model_run):
+        """
+        Increment the internal model run version counter by one.
+        """
+        self.set_model_run_version(model_run, self.get_model_run_version(model_run) + 1)
 
     def _serialize_model_run(self, value):
         return value.serialize() if self._valid_model_run(value) else None
@@ -346,6 +378,9 @@ class Daemon:
             if not model.is_complete_dataset(model_run.data):
                 logging.warn("Model run %s is not complete, discarding." % model_run.id)
                 return
+
+            model.increment_model_run_version(model_run)
+
         model.set_available_model_run(model_run)
         self.sync_zmq_status()
 

syncer/syncer/tests.py
@@ -349,6 +349,35 @@ class ModelTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             model.set_wdb2ts_model_run(model_run)
 
+    def test_get_model_run_key(self):
+        model = self.get_model()
+        model_run = self.get_model_run()
+        key = model.get_model_run_key(model_run)
+        self.assertEqual(len(key), 2)
+        self.assertEqual(key[0], 'arome_metcoop_2500m')
+        self.assertEqual(key[1].strftime('%Y-%m-%dT%H:%M:%SZ'), VALID_MODEL_RUN_FIXTURE['reference_time'])
+
+    def test_get_model_run_version(self):
+        model = self.get_model()
+        model_run = self.get_model_run()
+        version = model.get_model_run_version(model_run)
+        self.assertEqual(version, 0)
+
+    def test_set_model_run_version(self):
+        model = self.get_model()
+        model_run = self.get_model_run()
+        ref_version = 1337
+        model.set_model_run_version(model_run, ref_version)
+        version = model.get_model_run_version(model_run)
+        self.assertEqual(version, ref_version)
+
+    def test_increment_model_run_version(self):
+        model = self.get_model()
+        model_run = self.get_model_run()
+        model.increment_model_run_version(model_run)
+        version = model.get_model_run_version(model_run)
+        self.assertEqual(version, 1)
+
 
 class ModelRunTest(unittest.TestCase):
     """Tests the syncer.rest.ModelRun resource"""

syncer/syncer/wdb.py
@@ -130,7 +130,9 @@ class WDB(object):
         else:
             cmd.extend(["--loadPlaceDefinition"])
 
-        cmd.extend(["--dataversion", unicode(model_run.version)])
+        # compose a special version to ensure that reloads always replace data
+        version = model_run.version + model.get_model_run_version(model_run)
+        cmd.extend(["--dataversion", unicode(version)])
 
         cmd.append(model_file)
 

commit c06e15e2c258b815b238fe1bd3428a3b77a16ef9

Fix WDB2TS regression
Summary: This patch fixes T628. The WDB and WDB2TS data versions were out of sync. The regression was introduced in 08e11b7a.

Test Plan: Run the Syncer test suite. New tests have been added.

Reviewers: #nir, siris

Reviewed By: #nir, siris

Projects: #nir

Maniphest Tasks: T628

Differential Revision: https://phab.met.no/D368

syncer/syncer/__init__.py
@@ -240,6 +240,13 @@ class Model(syncer.utils.SerializeBase):
 
     def get_model_run_version(self, model_run):
         """
+        Return the definite version of the specified model run; a combination
+        of authoritative version and internal version.
+        """
+        return model_run.version + self.get_internal_model_run_version(model_run)
+
+    def get_internal_model_run_version(self, model_run):
+        """
         Return the internal version of the specified model run.
         """
         key = self.get_model_run_key(model_run)
@@ -251,7 +258,7 @@ class Model(syncer.utils.SerializeBase):
         """
         Increment the internal model run version counter by one.
         """
-        self.set_model_run_version(model_run, self.get_model_run_version(model_run) + 1)
+        self.set_model_run_version(model_run, self.get_internal_model_run_version(model_run) + 1)
 
     def _serialize_model_run(self, value):
         return value.serialize() if self._valid_model_run(value) else None

syncer/syncer/tests.py
@@ -457,13 +457,19 @@ class ModelTest(unittest.TestCase):
         model = self.get_model()
         model_run = self.get_model_run()
         version = model.get_model_run_version(model_run)
+        self.assertEqual(version, 1337)
+
+    def test_get_internal_model_run_version(self):
+        model = self.get_model()
+        model_run = self.get_model_run()
+        version = model.get_internal_model_run_version(model_run)
         self.assertEqual(version, 0)
 
     def test_set_model_run_version(self):
         model = self.get_model()
         model_run = self.get_model_run()
-        ref_version = 1337
-        model.set_model_run_version(model_run, ref_version)
+        ref_version = 2674
+        model.set_model_run_version(model_run, 1337)
         version = model.get_model_run_version(model_run)
         self.assertEqual(version, ref_version)
 
@@ -472,7 +478,7 @@ class ModelTest(unittest.TestCase):
         model_run = self.get_model_run()
         model.increment_model_run_version(model_run)
         version = model.get_model_run_version(model_run)
-        self.assertEqual(version, 1)
+        self.assertEqual(version, 1338)
 
 
 class ModelRunTest(unittest.TestCase):

syncer/syncer/wdb.py
@@ -130,8 +130,7 @@ class WDB(object):
         else:
             cmd.extend(["--loadPlaceDefinition"])
 
-        # compose a special version to ensure that reloads always replace data
-        version = model_run.version + model.get_model_run_version(model_run)
+        version = model.get_model_run_version(model_run)
         cmd.extend(["--dataversion", unicode(version)])
 
         cmd.append(model_file)

syncer/syncer/wdb2ts.py
@@ -105,7 +105,8 @@ class WDB2TS(object):
         """
         try:
             reference_time = model_run.serialize_reference_time(model_run.reference_time)  # wdb2ts is very picky about this
-            update_url = self.get_update_url(service, model.get_data_provider_or_group(), reference_time, model_run.version)
+            version = model.get_model_run_version(model_run)
+            update_url = self.get_update_url(service, model.get_data_provider_or_group(), reference_time, version)
         except TypeError, e:
             raise syncer.exceptions.WDB2TSClientUpdateFailure("Could not generate a correct update URL for WDB2TS: %s" % e)
 