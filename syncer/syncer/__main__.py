46196bfe51373d66364db3e1a123816fdc0662a1
Vegard Bønes <vegard.bones@met.no>
2018-05-24 08:39:49 +0200

    Correct exit status

--- syncer/syncer/__main__.py
+++ syncer/syncer/__main__.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 
+import logging
 import sys
 import syncer
 
 if __name__ == '__main__':
-    syncer.main(sys.argv[1:])
+    exit_code = syncer.main(sys.argv[1:])
+    logging.info("Exiting with status code %d", exit_code)
+    sys.exit(exit_code)
